5d30363322ef4e8f32b364c3027bed28cbaae9c5
Felix Mario <felix.fex2019@gmail.com>
2024-08-06 21:13:40 +0100

    Projecto simples de Processamento

---
+++ index.js
@@ -0,0 +1,20 @@
+const process = document.querySelectorAll('.process .processbar');
+const totalBars = process.length;
+const animationDuration = 2000; // Duração total da animação de uma barra
+const nextBarDelay = 1500;
+let currentBar = 0;
+
+setInterval(() => {
+  const bar = process[currentBar];
+  bar.classList.add('anima');
+
+  // Calcula o tempo para iniciar a próxima animação
+  const nextBarDelay = animationDuration / 2; // Metade da duração da animação
+
+  // Remove a classe 'anima' após a animação terminar
+  setTimeout(() => {
+    bar.classList.remove('anima');
+    currentBar = (currentBar + 1) % totalBars; // Incrementa o índice da próxima barra, circular
+  }, animationDuration);
+
+}, nextBarDelay);
